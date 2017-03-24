d9e025cc08cab690476a81d8355f246d8d872413
ShenLiangwei <shinelw.cool@gmail.com>
2017-03-24 16:48:37 +0800

    [update]network change

---
+++ app/src/main/java/com/peixuan/android_o_sample/NetworkingActivity.java
@@ -0,0 +1,13 @@
+package com.peixuan.android_o_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_networking);
+    }
+}
